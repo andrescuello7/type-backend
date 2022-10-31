48873fa756deede016e005d84f584e2fe3ee5180
andrescuello7 <andrescuellotrabajo@gmail.com>
2022-10-31 12:08:50 -0300

    deploy production

--- public/utils/manager/data_services.js
+++ public/utils/manager/data_services.js
@@ -1,11 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.DataService = void 0;
-const mongo_db_1 = require("../database/mongo/mongo_db");
+const mysql_db_1 = require("../database/mysql/mysql_db");
 class DataService {
     constructor() {
-        // this.connection = new MySqlConnection()
-        this.connection = new mongo_db_1.MongoDbConnection();
+        this.connection = new mysql_db_1.MySqlConnection();
+        // this.connection = new MongoDbConnection()
     }
 }
 exports.DataService = DataService;
